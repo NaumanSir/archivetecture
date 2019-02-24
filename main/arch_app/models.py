296491a6ec5ad5d9d7b60d0948a3d5b25558af9d
Nauman Husnani <naumanhusnani@gmail.com>
2019-02-24 16:06:33 -0600

    working on uploading and displaying images; redid migrations to accomodate such

--- main/arch_app/models.py
+++ main/arch_app/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class ArchManager(models.Manager):
+    def create_arch(self, data):
+        print(data)
+
 class Arch(models.Model):
     name = models.CharField(max_length=255)
     yrbuilt = models.IntegerField()
     location = models.CharField(max_length=255)
     desc = models.CharField(max_length=4000)
-    # objects = ArchManager()
+    image = models.CharField(max_length=255)
+
+    objects = ArchManager()
